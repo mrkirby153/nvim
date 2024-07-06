b73fa917a925ae933650661521a93b81e412f911
mrkirby153 <mr.austinwhyte@gmail.com>
2024-07-05 20:07:57 -0700

    Fix tabular plugin

--- cfg/plugins.nix
+++ cfg/plugins.nix
@@ -1,4 +1,18 @@
 {pkgs, ...}:
+let 
+  # TODO (2024-07-05): The 2024-07-03 plugin pulled in by nixpkgs-unstable has a bug that prevnets the plugin from working.
+  # This was fixed in https://github.com/godlygeek/tabular/commit/12437cd1b53488e24936ec4b091c9324cafee311 but is not updated
+  # in nixpkgs.
+  tabular-custom = pkgs.vimPlugins.tabular.overrideAttrs (oldAttrs: {
+    version = "2024-07-05";
+    src = pkgs.fetchFromGitHub {
+      owner = "godlygeek";
+      repo = "tabular";
+      rev = "12437cd1b53488e24936ec4b091c9324cafee311";
+      sha256 = "sha256-qmDpdg3Tl3W4JSovRb4ODlrKMjRL5CaVI05YBn0Q0LI=";
+    };
+  });
+in 
 {
 config.nvim.plugins = with pkgs.vimPlugins; [
   cmp-nvim-lsp
@@ -27,7 +41,7 @@ config.nvim.plugins = with pkgs.vimPlugins; [
   python-mode
   rainbow
   syntastic
-  tabular
+  tabular-custom
   tagbar
   telescope-nvim
   telescope-recent-files
